5734ed72721ef89dbe8b1654de47992d0c5cbb09
Scrontoe <brthomiu@gmail.com>
2022-05-30 17:26:30 -0600

    Inital rest API

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,12 +3,12 @@ const { appendFile } = require('fs')
 const router = express.Router()
 const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-router.get('/', getGoals)
+router.route('/')
+    .get(getGoals)
+    .post(setGoal)
 
-router.post('/', setGoal)
-
-router.put('/:id', updateGoal)
-
-router.delete('/:id', deleteGoal)
+router.route('/:id')
+    .put(updateGoal)
+    .delete(deleteGoal)
 
 module.exports = router
