2ffd23c2898367c456760e6d9e13000b919cb2b7
Scrontoe <brthomiu@gmail.com>
2022-05-30 15:39:34 -0600

    Initial routes and controllers set up

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Hello')
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv').config( {
+    path: path.join(__dirname, '.env')
+})
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.listen(port, () =>
+    console.log(`Server running on port ${port}`))
